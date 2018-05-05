d13ecca81a2199b8c232ea222b6ea914ecfc276d
Bogdan Ciobotaru <bogdan.ciobotaru1@gmail.com>
2018-05-05 17:15:37 +0200

    server can handle CORS

--- server/server.js
+++ server/server.js
@@ -31,7 +31,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '../public')));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-
+//CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 // Define routes.
 app.use('/', require('./routes'));
 
